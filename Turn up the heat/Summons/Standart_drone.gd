20c30639afa9e3f29b385e488d224bba5fc0f26e
NikitinMaksim <131441459+NikitinMaksim@users.noreply.github.com>
2024-04-23 20:58:57 +0500

    New GunData

--- Turn up the heat/Summons/Standart_drone.gd
+++ Turn up the heat/Summons/Standart_drone.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @export var target: Node2D
-
+@export var weapon: GunData
 var d = 0
 var speed: int = 2
 var radius: int = 300
@@ -10,7 +10,7 @@ func _ready():
 	$AnimationPlayer.active = true
 	$AnimationPlayer.current_animation = "Idle"
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	d+=delta
 	position = (Vector2(sin(d*speed)*radius,cos(d*speed)*radius)+target.global_position)
+	#TODO перенести сюда код стрельбы использя GunData
