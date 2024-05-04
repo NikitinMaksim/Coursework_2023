a99bfdb3af83bf8efd895d62f08cf552f889fe55
NikitinMaksim <131441459+NikitinMaksim@users.noreply.github.com>
2024-05-05 01:06:05 +0500

    MetaSaveDataResource

--- Turn up the heat/Resources/templates/MetaUpgrades.gd
+++ Turn up the heat/Resources/templates/MetaUpgrades.gd
@@ -3,3 +3,15 @@ extends Resource
 class_name MetaUpgradesStats
 
 @export var total_points: int = 0
+@export var damage_levels: int = 0
+@export var max_damage_levels: int = 3
+@export var armor_levels: int = 0
+@export var max_armor_levels: int = 3
+@export var speed_levels: int = 0
+@export var max_speed_levels: int = 3
+@export var projectile_count_levels: int = 0
+@export var max_projectile_count_levels: int = 3
+@export var ammo_levels: int = 0
+@export var max_ammo_levels: int = 3
+@export var fuel_levels: int = 0
+@export var max_fuel_levels: int = 3
