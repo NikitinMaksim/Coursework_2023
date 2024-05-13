cb9f9e8ecbfc09f2537c465d4e8bc4b937238677
NikitinMaksim <131441459+NikitinMaksim@users.noreply.github.com>
2024-05-13 20:59:52 +0500

    Shotgun + grenade

---
+++ Turn up the heat/Resources/Bullets/grenade.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+#TODO наносит 1/5 урона при столкновении, полный урон по области или когда кончаются пробития и происходит столкновение или когда полностью останавливается
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
