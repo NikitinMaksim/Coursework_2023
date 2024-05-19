631b4301f332125369b4aeb90031881b33a2b447
NikitinMaksim <131441459+NikitinMaksim@users.noreply.github.com>
2024-05-20 02:41:37 +0500

    Grenade launcher ready

--- Turn up the heat/Resources/Bullets/grenade.gd
+++ Turn up the heat/Resources/Bullets/grenade.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+@onready var detonation_timer = $DetonationTimer
+@onready var explosion_area = $ExplosionArea
+@onready var touch_area = $TouchArea
+
 @export var speed: int = 750
 @export var max_distance: int = 100
 @export var max_pierce: int = 0
@@ -21,8 +25,30 @@ func _ready():
 #TODO наносит 1/5 урона при столкновении, полный урон по области или когда кончаются пробития и происходит столкновение или когда полностью останавливается
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#$GPUParticles2D.emitting = true
-
+	position += transform.x * speed * delta
+	remaining_distance -= speed*delta
+	if (remaining_distance<0 and speed>0): 
+		speed = clamp(speed-50,0,speed)
+	if speed == 0 and detonation_timer.is_stopped():
+		end_of_life()
 
 func _on_touch_area_body_entered(body):
-	pass # Replace with function body.
+	if body is HitboxComponent:
+		var damage = attack/5*(1+ramp_up*0.15)
+		body.damage(damage)
+	current_pierce -= 1
+	if current_pierce<0:
+		end_of_life()
+		_on_detonation_timer_timeout()
+
+func end_of_life():
+	$GPUParticles2D.emitting = true
+	$GPUParticles2D.reparent(get_parent())
+	detonation_timer.start()
+
+func _on_detonation_timer_timeout():
+	for enemy in explosion_area.get_overlapping_bodies():
+		if enemy is HitboxComponent:
+			var damage = attack*(1+ramp_up*0.15)
+			enemy.damage(damage)
+	queue_free()
